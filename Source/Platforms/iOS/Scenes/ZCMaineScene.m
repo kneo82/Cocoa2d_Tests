06d5d1b748d5f81885265c7437fc8bda8a3a5ced
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-02-02 09:20:48 +0200

    Add ZCMaineScene set on DebugMode

---
+++ Source/Platforms/iOS/Scenes/ZCMaineScene.m
@@ -0,0 +1,13 @@
+//
+//  ZCMaineScene.m
+//  ZombiConga_Cocoa2d
+//
+//  Created by Voronok Vitaliy on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "ZCMaineScene.h"
+
+@implementation ZCMaineScene
+
+@end

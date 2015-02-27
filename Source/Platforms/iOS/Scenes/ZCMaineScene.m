162afa95438a26fbf4f76ee9104283c40220c48d
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-02-27 14:38:24 +0200

    add physics for circle

--- Source/Platforms/iOS/Scenes/ZCMaineScene.m
+++ Source/Platforms/iOS/Scenes/ZCMaineScene.m
@@ -10,11 +10,13 @@
 #import "CGGeometry+ZCExtension.h"
 
 @interface ZCMaineScene ()
-@property (nonatomic, assign)   CGSize  winSize;
+@property (nonatomic, assign)   CGSize          winSize;
 
-@property (nonatomic, strong)   CCSprite    *square;
-@property (nonatomic, strong)   CCSprite    *circle;
-@property (nonatomic, strong)   CCSprite    *triangle;
+@property (nonatomic, strong)   CCPhysicsNode   *physicWorld;
+
+@property (nonatomic, strong)   CCSprite        *square;
+@property (nonatomic, strong)   CCSprite        *circle;
+@property (nonatomic, strong)   CCSprite        *triangle;
 
 - (void)setupSprites;
 
@@ -30,12 +32,21 @@
     
     if (self) {
         self.winSize = [CCDirector sharedDirector].viewSize;
+        NSLog(@"---- %@", NSStringFromCGSize(self.winSize));
         
+        CCPhysicsNode* physicsNode = [CCPhysicsNode node];
+        [self addChild:physicsNode z:10];
+        physicsNode.gravity = ccp(0,-10);
         
+        physicsNode.debugDraw = YES;
+        
+        self.physicWorld = physicsNode;
+        
+
         ccColor4B color = {50, 45, 30, 255};
         CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithCcColor4b:color]];
 
-        [self addChild:background];
+        [self addChild:background z:-10];
         
         self.userInteractionEnabled = YES;
         
@@ -73,15 +84,32 @@
     NSLog(@"Touch Moved : (%@)", NSStringFromCGPoint(touchLocation));
 }
 
+
 #pragma mark -
 #pragma mark Private
 
 - (void)setupSprites {
-    CCSprite *square = [CCSprite spriteWithImageNamed:@"square@1x.png"];
+    
+    CCSprite *square = [CCSprite spriteWithImageNamed:@"square.png"];
     square.position = ccp(self.winSize.width * 0.25, self.winSize.height * 0.50);
     self.square = square;
     
-    [self addChild:square];
+    [self.physicWorld addChild:square];
+    
+    CCSprite *circle = [CCSprite spriteWithImageNamed:@"circle.png"];
+    circle.position = ccp(self.winSize.width * 0.5, self.winSize.height * 0.50);
+    CGFloat radius = (circle.contentSize.width / 2) ;
+    circle.physicsBody = [CCPhysicsBody bodyWithCircleOfRadius:radius andCenter:ccp(radius, radius)];
+    
+    self.circle = circle;
+    
+    [self.physicWorld addChild:circle];
+    
+    CCSprite *triangle = [CCSprite spriteWithImageNamed:@"triangle.png"];
+    triangle.position = ccp(self.winSize.width * 0.75, self.winSize.height * 0.50);
+    self.triangle = triangle;
+    
+    [self.physicWorld addChild:triangle];
 }
 
 @end

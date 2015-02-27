88be7de515e335d09e93c50926c5b4acf4c3a689
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-02-27 09:48:43 +0200

    no message

--- Source/Platforms/iOS/Scenes/ZCMaineScene.m
+++ Source/Platforms/iOS/Scenes/ZCMaineScene.m
@@ -10,7 +10,13 @@
 #import "CGGeometry+ZCExtension.h"
 
 @interface ZCMaineScene ()
+@property (nonatomic, assign)   CGSize  winSize;
 
+@property (nonatomic, strong)   CCSprite    *square;
+@property (nonatomic, strong)   CCSprite    *circle;
+@property (nonatomic, strong)   CCSprite    *triangle;
+
+- (void)setupSprites;
 
 @end
 
@@ -23,7 +29,17 @@
     self = [super init];
     
     if (self) {
+        self.winSize = [CCDirector sharedDirector].viewSize;
+        
+        
+        ccColor4B color = {50, 45, 30, 255};
+        CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor colorWithCcColor4b:color]];
+
+        [self addChild:background];
+        
         self.userInteractionEnabled = YES;
+        
+        [self setupSprites];
     }
     
     return self;
@@ -59,5 +75,13 @@
 
 #pragma mark -
 #pragma mark Private
+
+- (void)setupSprites {
+    CCSprite *square = [CCSprite spriteWithImageNamed:@"square@1x.png"];
+    square.position = ccp(self.winSize.width * 0.25, self.winSize.height * 0.50);
+    self.square = square;
+    
+    [self addChild:square];
+}
 
 @end
